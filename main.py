6d390776d7c5d5e5ae96e8f4dd310ebc29c25788
Matthew Evans <me388@cam.ac.uk>
2020-06-09 16:32:46 +0100

    init

---
+++ main.py
@@ -0,0 +1,109 @@
+# pylint: disable=line-too-long
+import json
+import os
+from pathlib import Path
+
+from lark.exceptions import VisitError
+
+from pydantic import ValidationError
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError, StarletteHTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from optimade import __api_version__, __version__
+import optimade.server.exception_handlers as exc_handlers
+
+from optimade.server.entry_collections import MongoCollection
+from optimade.server.config import CONFIG
+from optimade.server.middleware import EnsureQueryParamIntegrity
+from optimade.server.routers import info, links, references, structures, landing
+from optimade.server.routers.utils import get_providers, BASE_URL_PREFIXES
+
+
+if CONFIG.debug:  # pragma: no cover
+    print("DEBUG MODE")
+
+
+app = FastAPI(
+    title="OPTIMADE API",
+    description=(
+        f"""The [Open Databases Integration for Materials Design (OPTIMADE) consortium](https://www.optimade.org/) aims
+to make materials databases interoperational by developing a common REST API.
+
+This specification is generated using
+[`optimade-python-tools`](https://github.com/Materials-Consortia/optimade-python-tools/tree/v{__version__}) v{__version__}.
+"""
+    ),
+    version=__api_version__,
+    docs_url=f"{BASE_URL_PREFIXES['major']}/extensions/docs",
+    redoc_url=f"{BASE_URL_PREFIXES['major']}/extensions/redoc",
+    openapi_url=f"{BASE_URL_PREFIXES['major']}/extensions/openapi.json",
+)
+
+
+if not CONFIG.use_real_mongo:
+    import bson.json_util
+    from optimade.server.routers import ENTRY_COLLECTIONS
+
+    data = {}
+    with open("./structures.json", "r") as f:
+        data["structures"] = bson.json_util.loads(f.read())
+    with open("./references.json", "r") as f:
+        data["references"] = bson.json_util.loads(f.read())
+
+    def load_entries(endpoint_name: str, endpoint_collection: MongoCollection):
+        print(f"loading test {endpoint_name}...")
+        if endpoint_name != "links":
+            endpoint_collection.collection.insert_many(data.get(endpoint_name, []))
+        if endpoint_name == "links":
+            print("adding Materials-Consortia providers to links from optimade.org")
+            endpoint_collection.collection.insert_many(
+                bson.json_util.loads(bson.json_util.dumps(get_providers()))
+            )
+        print(f"done inserting test {endpoint_name}...")
+
+    for name, collection in ENTRY_COLLECTIONS.items():
+        load_entries(name, collection)
+
+
+# Add various middleware
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+app.add_middleware(EnsureQueryParamIntegrity)
+
+
+# Add various exception handlers
+app.add_exception_handler(StarletteHTTPException, exc_handlers.http_exception_handler)
+app.add_exception_handler(
+    RequestValidationError, exc_handlers.request_validation_exception_handler
+)
+app.add_exception_handler(ValidationError, exc_handlers.validation_exception_handler)
+app.add_exception_handler(VisitError, exc_handlers.grammar_not_implemented_handler)
+app.add_exception_handler(NotImplementedError, exc_handlers.not_implemented_handler)
+app.add_exception_handler(Exception, exc_handlers.general_exception_handler)
+
+
+# Add various endpoints to `/vMAJOR`
+app.include_router(info.router, prefix=BASE_URL_PREFIXES["major"])
+app.include_router(links.router, prefix=BASE_URL_PREFIXES["major"])
+app.include_router(references.router, prefix=BASE_URL_PREFIXES["major"])
+app.include_router(structures.router, prefix=BASE_URL_PREFIXES["major"])
+
+
+# Add the router for the landing page for all prefixes
+app.include_router(landing.router)
+app.include_router(landing.router, prefix=BASE_URL_PREFIXES["major"])
+
+
+def update_schema(app: FastAPI):
+    """Update OpenAPI schema in file 'local_openapi.json'"""
+    package_root = Path(__file__).parent.parent.parent.resolve()
+    if not package_root.joinpath("openapi").exists():
+        os.mkdir(package_root.joinpath("openapi"))
+    with open(package_root.joinpath("openapi/local_openapi.json"), "w") as f:
+        json.dump(app.openapi(), f, indent=2)
+
+
+@app.on_event("startup")
+async def startup_event():
+    # Update OpenAPI schema on versioned base URL `/vMAJOR`
+    update_schema(app)
